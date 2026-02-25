11547513df86f25377b86df6312090974cdaa0b1
Leviathan-19 <msnarvaez19@hotmail.com>
2026-02-25 18:14:22 -0500

    add app.use("/api/establecimientos", establecimientoRoutes); from app.ts

--- ivanseg-backend/src/app.ts
+++ ivanseg-backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import clienteRoutes from "./routes/cliente.routes";
 import clienteEstablecimientoRoutes from "./routes/clienteEstablecimiento.routes";
 import mantenimientoRoutes from "./routes/mantenimiento.routes";
+import establecimientoRoutes from "./routes/establecimiento.routes";
 import { errorHandler } from "./middlewares/error.middleware";
 
 const app = express();
@@ -11,7 +12,7 @@ app.use(express.json());
 app.use("/api/clientes", clienteRoutes);
 app.use("/api/clientes-establecimientos", clienteEstablecimientoRoutes);
 app.use("/api/mantenimientos", mantenimientoRoutes);
-
+app.use("/api/establecimientos", establecimientoRoutes);
 app.use(errorHandler);
 
 export default app;
