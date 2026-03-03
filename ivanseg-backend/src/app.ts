3511455e2dbeaf6e6109b3aa08fed28e62f95450
Leviathan-19 <msnarvaez19@hotmail.com>
2026-03-03 00:55:19 -0500

    add cors from backend hardcoding from localhost:4200

--- ivanseg-backend/src/app.ts
+++ ivanseg-backend/src/app.ts
@@ -9,10 +9,15 @@ import parroquiaRoutes from "./routes/parroquia.route";
 import barrioRoutes from "./routes/barrio.route";
 import visitaRoutes from "./routes/visita.route";
 import { errorHandler } from "./middlewares/error.middleware";
-
+import cors from 'cors';
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:4200',
+  credentials: true
+}));
 
 app.use("/api/clientes", clienteRoutes);
 app.use("/api/clientes-establecimientos", clienteEstablecimientoRoutes);
